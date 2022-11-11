e7ee23cb14a4fb6073ca2a1c6acd6a7aa3f3eb9c
MHusna-Kurs <mehmethusnakurs@gmail.com>
2022-11-11 14:41:31 +0300

    11_11_2022

---
+++ Kasım/11_11_2022/11_11_2022/11_11_2022/Document.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11_11_2022
+{
+    public class Document
+    {
+        public virtual void Print()
+        {
+            Console.WriteLine("Document yazıldı.");
+        }
+    }
+}
---
+++ Kasım/11_11_2022/11_11_2022/11_11_2022/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11_11_2022
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Printer printer = new Printer();
+
+            printer.print(new Document());
+            printer.print(new Excel());
+            printer.print(new Word());
+
+            Console.ReadLine();
+        }
+    }
+}

ab8a035653f942f5c8c82128bb570b22443fcd7a
MHusna-Kurs <mehmethusnakurs@gmail.com>
2022-11-14 09:39:26 +0300

    14_11_2022 ornek 1 kodlandi.

---
+++ Kasım/14_11_2022/14_11_2022/14_11_2022/Circle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14_11_2022
+{
+    class Circle : IShape
+    {
+        public void draw()
+        {
+            Console.WriteLine("draw() from circle.");
+        }
+    }
+}
---
+++ Kasım/14_11_2022/14_11_2022/14_11_2022/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14_11_2022
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IShape shape;
+            
+            shape = new Circle();
+            shape.draw();
+
+            shape = new Square();
+            shape.draw();
+
+            shape = new Rectangle();
+            shape.draw();
+
+            Console.ReadLine();
+        }
+    }
+}
